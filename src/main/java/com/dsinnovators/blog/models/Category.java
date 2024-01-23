ffcd39ac7ecb81bcbbeb9f5ab6d21d951c949496
Md. Abdus Salam <salaam.mbstu@gmail.com>
2024-01-23 19:24:47 +0600

    Add category entity, table, controller and view

---
+++ src/main/java/com/dsinnovators/blog/models/Category.java
@@ -0,0 +1,40 @@
+package com.dsinnovators.blog.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "categories_seq")
+    @SequenceGenerator(name = "categories_seq", sequenceName = "categories_seq", allocationSize = 1)
+    private Long id;
+    private String name;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
