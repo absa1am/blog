b827703dcffd3f3cad4bf3c782ba1f2b0282e55d
Md. Abdus Salam <salaam.mbstu@gmail.com>
2024-01-24 19:04:15 +0600

    Add post feature

--- src/main/java/com/dsinnovators/blog/controllers/PostController.java
+++ src/main/java/com/dsinnovators/blog/controllers/PostController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -20,6 +21,15 @@ public class PostController {
     private PostService postService;
     @Autowired
     private CategoryService categoryService;
+
+    @GetMapping("/posts")
+    public String index(Model model) {
+        List<Post> posts = postService.getAll();
+
+        model.addAttribute("posts", posts);
+
+        return "post/index";
+    }
 
     @GetMapping("/post/create")
     public String create(Model model) {
@@ -36,6 +46,24 @@ public class PostController {
         postService.save(post);
 
         return "redirect:/post/create";
+    }
+
+    @GetMapping("/post/{id}/update")
+    public String update(Model model, @PathVariable Long id) {
+        Post post = postService.find(id);
+        List<Category> categories = categoryService.getAll();
+
+        model.addAttribute("post", post);
+        model.addAttribute("categories", categories);
+
+        return "post/update";
+    }
+
+    @PostMapping("/post/{id}/update")
+    public String update(@ModelAttribute Post post, @PathVariable Long id) {
+        postService.update(post, id);
+
+        return "redirect:/posts";
     }
 
 }
--- src/main/java/com/dsinnovators/blog/services/PostService.java
+++ src/main/java/com/dsinnovators/blog/services/PostService.java
@@ -5,14 +5,35 @@ import com.dsinnovators.blog.repositories.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PostService {
 
     @Autowired
     private PostRepository postRepository;
 
+    public List<Post> getAll() {
+        return postRepository.findAll();
+    }
+
+    public Post find(Long id) {
+        return postRepository.findById(id).get();
+    }
+
     public Post save(Post post) {
         return postRepository.save(post);
+    }
+
+    public Post update(Post post, Long id) {
+        Post oldPost = postRepository.findById(id).get();
+
+        oldPost.setId(post.getId());
+        oldPost.setTitle(post.getTitle());
+        oldPost.setDescription(post.getDescription());
+        oldPost.setCategory(post.getCategory());
+
+        return postRepository.save(oldPost);
     }
 
 }
